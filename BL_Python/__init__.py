fadc0d85ec25f6fc673e084bafd623d94963bd13
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-08-03 16:37:26 -0700

    Remove setup.cfg and use only pyproject.toml.

--- BL_Python/__init__.py
+++ BL_Python/__init__.py
@@ -1,12 +1,1 @@
-__version__: str = ""
-
-try:
-    import importlib.metadata
-
-    __version__ = importlib.metadata.version(__package__ or __name__)
-except:
-    from pip._vendor import tomli
-
-    with open("../pyproject.toml") as f:
-        t = tomli.loads(f.read())
-        __version__ = t["project"]["version"]
+__version__: str = "0.0.1"
