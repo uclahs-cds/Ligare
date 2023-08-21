985dc3b927b6ba50d5117ced641a3bed5b98bbb6
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-08-21 14:18:34 -0700

    Supporting updating env to better support Flask config

--- src/web/BL_Python/web/config.py
+++ src/web/BL_Python/web/config.py
@@ -1,7 +1,9 @@
+from os import environ
 from typing import Any, Optional
 
 import toml
 from BL_Python.programming.collections.dict import AnyDict, merge
+from flask.config import Config as FlaskAppConfig
 from pydantic.dataclasses import dataclass
 
 
@@ -12,7 +14,7 @@ class LoggingConfig:
 
 @dataclass(frozen=True)
 class FlaskOpenApiConfig:
-    spec_path: Optional[str] = None
+    spec_path: str | None = None
     validate_responses: bool = False
 
 
@@ -23,23 +25,84 @@ class FlaskSessionCookieConfig:
     secure: bool = True
     samesite: str = "None"
 
+    def _prepare_env_for_flask(self):
+        environ.update(
+            {
+                "SECRET_KEY": self.secret_key,
+                "SESSION_COOKIE_HTTPONLY": str(1 if self.httponly else 0),
+                "SESSION_COOKIE_SECURE": str(1 if self.secure else 0),
+                "SESSION_COOKIE_SAMESITE": self.samesite,
+            }
+        )
+
+    def _update_flask_config(self, flask_app_config: FlaskAppConfig):
+        class ConfigObject:
+            SECRET_KEY = self.secret_key
+            SESSION_COOKIE_HTTPONLY = self.httponly
+            SESSION_COOKIE_SECURE = self.secure
+            SESSION_COOKIE_SAMESITE = self.samesite
+
+        flask_app_config.from_object(ConfigObject)
+
 
 @dataclass(frozen=True)
 class FlaskSessionConfig:
     cookie: FlaskSessionCookieConfig
     permanent: bool = True
-    lifetime: int = 86400
+    lifetime: int | None = 86400
     refresh_each_request: bool = True
+
+    def _prepare_env_for_flask(self):
+        environ.update(
+            {
+                "PERMANENT_SESSION": str(1 if self.permanent else 0),
+                "PERMANENT_SESSION_LIFETIME": str(self.lifetime)
+                if self.lifetime
+                else "",
+                "SESSION_REFRESH_EACH_REQUEST": str(
+                    1 if self.refresh_each_request else 0
+                ),
+            }
+        )
+        self.cookie._prepare_env_for_flask()  # pyright: ignore[reportPrivateUsage]
+
+    def _update_flask_config(self, flask_app_config: FlaskAppConfig):
+        self.cookie._update_flask_config(  # pyright: ignore[reportPrivateUsage]
+            flask_app_config
+        )
+
+        class ConfigObject:
+            PERMANENT_SESSION = self.permanent
+            PERMANENT_SESSION_LIFETIME = self.lifetime
+            SESSION_REFRESH_EACH_REQUEST = self.refresh_each_request
+
+        flask_app_config.from_object(ConfigObject)
 
 
 @dataclass(frozen=True)
 class FlaskConfig:
     app_name: str = "app"
     env: str = "Development"
-    host: Optional[str] = None
-    port: Optional[str] = None
-    openapi: Optional[FlaskOpenApiConfig] = None
-    session: Optional[FlaskSessionConfig] = None
+    host: str | None = None
+    port: str | None = None
+    openapi: FlaskOpenApiConfig | None = None
+    session: FlaskSessionConfig | None = None
+
+    def _prepare_env_for_flask(self):
+        environ.update({"ENV": self.env})
+        if self.session:
+            self.session._prepare_env_for_flask()  # pyright: ignore[reportPrivateUsage]
+
+    def _update_flask_config(self, flask_app_config: FlaskAppConfig):
+        if self.session:
+            self.session._update_flask_config(  # pyright: ignore[reportPrivateUsage]
+                flask_app_config
+            )
+
+        class ConfigObject:
+            ENV = self.env
+
+        flask_app_config.from_object(ConfigObject)
 
 
 @dataclass(frozen=True)
@@ -50,26 +113,38 @@ class DatabaseConfig:
 
 @dataclass(frozen=True)
 class SAML2Config:
-    metadata: Optional[str] = None
-    metadata_url: Optional[str] = None
-    relay_state: Optional[str] = None
+    metadata: str | None = None
+    metadata_url: str | None = None
+    relay_state: str | None = None
     # TODO is there a more specific type than `Any`
     # that fits logging configurations?
-    logging: Optional[dict[str, Any]] = None
+    logging: dict[str, Any] | None = None
 
 
 @dataclass(frozen=True)
 class Config:
     logging: LoggingConfig = LoggingConfig()
-    flask: Optional[FlaskConfig] = None
-    database: Optional[DatabaseConfig] = None
-    saml2: Optional[SAML2Config] = None
+    flask: FlaskConfig | None = None
+    database: DatabaseConfig | None = None
+    saml2: SAML2Config | None = None
+
+    def prepare_env_for_flask(self):
+        if self.flask:
+            self.flask._prepare_env_for_flask()  # pyright: ignore[reportPrivateUsage]
+
+    def update_flask_config(self, flask_app_config: FlaskAppConfig):
+        if self.flask:
+            self.flask._update_flask_config(  # pyright: ignore[reportPrivateUsage]
+                flask_app_config
+            )
 
 
-def load_config(toml_file_path: str, config_overrides: Optional[AnyDict] = None):
+def load_config(toml_file_path: str, config_overrides: AnyDict | None = None):
     config_dict: dict[str, Any] = toml.load(toml_file_path)
 
     if config_overrides is not None:
         config_dict = merge(config_dict, config_overrides)
 
-    return Config(**config_dict)
+    config = Config(**config_dict)
+    config.prepare_env_for_flask()
+    return config
