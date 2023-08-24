6c466000b4d8d0c77f9ec5933685b8fca74f15cc
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-08-23 19:10:10 -0700

    Support easy logging to stdout.

--- src/programming/BL_Python/programming/patterns/dependency_injection.py
+++ src/programming/BL_Python/programming/patterns/dependency_injection.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from typing import Any
 
@@ -6,10 +7,14 @@ from typing_extensions import override
 
 
 class LoggerModule(Module):
-    def __init__(self, name: str | None = None, log_level: int = logging.INFO) -> None:
+    def __init__(self, name: str | None = None, log_level: int = logging.INFO, log_to_stdout: bool = False) -> None:
         super().__init__()
         self._logger = logging.getLogger(name)
         self._logger.setLevel(log_level)
+        if log_to_stdout:
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(log_level)
+            self._logger.addHandler(handler)
 
     @override
     def configure(self, binder: Binder) -> None:
