fc546eba8ff6d5800c54c8e85c2d74a9b71668fa
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-09-13 11:56:20 -0700

    Add additional scaffolding options. Add script to pyproject. Add jinja templates.

--- src/web/BL_Python/web/scaffolding/__main__.py
+++ src/web/BL_Python/web/scaffolding/__main__.py
@@ -1,2 +1,38 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="BL_Python Web Project Scaffolding Tool"
+    )
+    _ = parser.add_argument(
+        "-n",
+        metavar="name",
+        dest="name",
+        required=True,
+        type=str,
+        help="The name of the application.",
+    )
+    template_types = ["basic", "openapi"]
+    _ = parser.add_argument(
+        "-t",
+        choices=template_types,
+        type=str,
+        default="basic",
+        help="The type of template to scaffold.",
+    )
+    modules = ["database"]
+    _ = parser.add_argument(
+        "-m",
+        choices=modules,
+        action="append",
+        type=str,
+        help="Optional modules to include in the application.",
+    )
+
+    return vars(parser.parse_args())
+
+
 def scaffold():
-    print("Hello!")
+    args = parse_args()
+    print(args)
