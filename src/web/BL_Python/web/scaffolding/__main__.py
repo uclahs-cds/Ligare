0cbe108accc6eccdc19759a52db77e45ecc7bdf1
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-09-14 16:20:37 -0700

    Change how optional templates are rendered.

--- src/web/BL_Python/web/scaffolding/__main__.py
+++ src/web/BL_Python/web/scaffolding/__main__.py
@@ -1,10 +1,9 @@
 import logging
 from argparse import ArgumentParser, Namespace
 from os import environ
-from typing import cast
+from typing import Any, cast
 
-from jinja2 import nodes
-from jinja2.ext import Extension
+from jinja2 import Template
 
 if environ.get("LOG_LEVEL"):
     logging.basicConfig(
@@ -69,12 +68,12 @@ def parse_args():
     return args
 
 
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass
 class ScaffoldModules:
-    database = False
+    database: bool = True
 
 
 @dataclass
@@ -100,43 +99,62 @@ def scaffold_template(args: Namespace):
             else:
                 log.debug(f"Creating directory `{directory}`.")
                 directory.mkdir(parents=True, exist_ok=True)
-        pass
 
-    env = Environment(
-        trim_blocks=True,
-        lstrip_blocks=True,
-        loader=PackageLoader("BL_Python.web", f"scaffolding/templates/{template_type}"),
-    )
-
-    should_skip = False
-
-    # the use of this prevent parallelizing template rendering
-    def skip_file(skip: bool):
-        nonlocal should_skip
-        should_skip = skip
-        return
-
-    env.globals["skip_file"] = skip_file
-
-    template_config = ScaffoldConfig(modules=ScaffoldModules())
-    # a list of directories already checked for existence
-    for template_name in cast(list[str], env.list_templates()):
-        should_skip = False
-        template_output_path = Path(
-            args.output_directory, template_name.replace(".j2", "")
-        )
+    def render_template(
+        template_name: str,
+        output_directory: str,
+        template_environment: Environment,
+        template_config: dict[str, Any],
+        template: Template | None = None,
+    ):
+        template_output_path = Path(output_directory, template_name.replace(".j2", ""))
 
         create_directory(template_output_path.parent)
 
         log.debug(
             f"Rendering template `{template_output_path}`. Using config `{template_config}`"
         )
-        r = env.get_template(template_name).render(**template_config.__dict__)
-        # stream = env.get_template(template_name).stream(**template_config.__dict__)
-        if not should_skip:
-            with open(template_output_path, "w") as f:
-                f.write(r)
-            # stream.dump(str(template_output_path))
+
+        if not template:
+            template = template_environment.get_template(template_name)
+
+        template.stream(**template_config).dump(str(template_output_path))
+
+    env = Environment(
+        trim_blocks=True,
+        lstrip_blocks=True,
+        loader=PackageLoader("BL_Python.web", f"scaffolding/templates/{template_type}"),
+    )
+    optional_env = Environment(
+        trim_blocks=True,
+        lstrip_blocks=True,
+        loader=PackageLoader("BL_Python.web", f"scaffolding/templates/optional"),
+    )
+
+    template_config = ScaffoldConfig(modules=ScaffoldModules())
+    template_config_dict = asdict(template_config)
+
+    # render the base templates
+    for template_name in cast(list[str], env.list_templates()):
+        render_template(template_name, args.output_directory, env, template_config_dict)
+
+    # render optional module templates
+    for module in asdict(template_config.modules):
+        module_is_enabled = getattr(template_config.modules, module)
+        if module_is_enabled:
+            template = optional_env.get_template(f"modules/{module}.py.j2")
+
+            if template.name is None:
+                log.error(f"Could not find template `modules/{module}.py.j2`.")
+                continue
+
+            render_template(
+                template.name,
+                args.output_directory,
+                env,
+                template_config_dict,
+                template,
+            )
 
 
 def scaffold():
