f72542437d4322d30f934bac1b6ab406ea980d1d
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-10-03 13:25:07 -0700

    Fix up web and database config unit tests.
    - Also fix an issue where `pytest` cannot import tests when file names are not globally unique by adding `--import-mode=importlib`

---
+++ src/database/test/unit/test_config.py
@@ -0,0 +1,12 @@
+from BL_Python.database.config import DatabaseConfig
+from BL_Python.programming.config import load_config
+from pytest_mock import MockerFixture
+
+
+def test__Config__load_config__initializes_database_config(mocker: MockerFixture):
+    fake_config_dict = {"connection_string": "test_connection_string"}
+    _ = mocker.patch("io.open")
+    _ = mocker.patch("toml.decoder.loads", return_value=fake_config_dict)
+    config = load_config(DatabaseConfig, "foo.toml")
+    assert config is not None
+    assert config.connection_string == "test_connection_string"
--- src/web/test/unit/test_config.py
+++ src/web/test/unit/test_config.py
@@ -25,21 +25,3 @@ def test__Config__load_config__initializes_flask_config(mocker: MockerFixture):
     config = load_config(Config, "foo.toml")
     assert config.flask is not None
     assert config.flask.app_name == "test_app"
-
-
-# def test__Config__load_config__initializes_database_config(mocker: MockerFixture):
-#    fake_config_dict = {"database": {"connection_string": "test_connection_string"}}
-#    _ = mocker.patch("io.open")
-#    _ = mocker.patch("toml.decoder.loads", return_value=fake_config_dict)
-#    config = load_config("foo.toml", config_type=Config)
-#    assert config.database is not None
-#    assert config.database.connection_string == "test_connection_string"
-
-
-# def test__Config__load_config__initializes_SAML2_config(mocker: MockerFixture):
-#    fake_config_dict = {"saml2": {"metadata": "test_metadata"}}
-#    _ = mocker.patch("io.open")
-#    _ = mocker.patch("toml.decoder.loads", return_value=fake_config_dict)
-#    config = load_config("foo.toml", config_type=Config)
-#    assert config.saml2 is not None
-#    assert config.saml2.metadata == "test_metadata"
