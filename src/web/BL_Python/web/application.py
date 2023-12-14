787ddd042237a26d1c55f53aa62466051988c3fd
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-12-14 13:25:09 -0800

    Fix issue with `BL_Python.web.config.Config` being registered in IoC as the wrong type.
    This was causing the application to load the default Config rather than
    the correctly instantiated one.
    
    Related #33

--- src/web/BL_Python/web/application.py
+++ src/web/BL_Python/web/application.py
@@ -119,7 +119,7 @@ def create_app(
     # The `full_config` module cannot be overridden unless the application
     # IoC container is fiddled with. `full_config` is the instance registered
     # to `AbstractConfig`.
-    modules = application_modules + [ConfigModule(full_config)]
+    modules = application_modules + [ConfigModule(full_config, type(full_config))]
     flask_injector = configure_dependencies(app, application_modules=modules)
     app.injector = flask_injector  # pyright: ignore[reportGeneralTypeIssues]
 
