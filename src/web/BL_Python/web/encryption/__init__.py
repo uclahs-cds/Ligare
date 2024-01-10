a17bfe669646570f09fab3772f533130d64e6115
Aaron Holmes <aholmes@mednet.ucla.edu>
2024-01-09 16:36:57 -0800

    Add tests for encryption and dependency injection.
    Related #33

--- src/web/BL_Python/web/encryption/__init__.py
+++ src/web/BL_Python/web/encryption/__init__.py
@@ -8,6 +8,10 @@ from itsdangerous import URLSafeTimedSerializer
 if TYPE_CHECKING:
     from _typeshed import ReadableBuffer  # pragma: no cover
 
+_EMPTY_SECRET_KEY_MESSAGE = (
+    "It is insecure to use an empty `secret_key`. Refusing to continue."
+)
+
 
 def _get_serializer(
     secret_key: str,
@@ -34,6 +38,9 @@ def decrypt_flask_cookie(secret_key: str, cookie_str: str) -> Dict[str, Any]:
     Decrypt a Flask session cookie.
     https://gist.github.com/babldev/502364a3f7c9bafaa6db
     """
+    if not secret_key:
+        raise Exception(_EMPTY_SECRET_KEY_MESSAGE)
+
     serializer = _get_serializer(secret_key)
     data: Any = serializer.loads(cookie_str)
     assert type(data) is dict
@@ -44,6 +51,9 @@ def encrypt_flask_cookie(secret_key: str, data: Dict[str, Any] | SessionMixin) -
     """
     Encrypt a dictionary into a Flask session cookie.
     """
+    if not secret_key:
+        raise Exception(_EMPTY_SECRET_KEY_MESSAGE)
+
     serializer = _get_serializer(secret_key)
     cookie_str = serializer.dumps(data)
     assert type(cookie_str) is str
