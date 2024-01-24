89440fc79c7cd53de4523bdef3ee12200e8cf999
Aaron Holmes <aholmes@mednet.ucla.edu>
2024-01-24 10:13:17 -0800

    Change how CICD restores and saves the build cache, and cache pyright.
    * Always restore the build cache for non-`workflow_dispatch` events.
    * Added input `use_build_cache` to control whether a `workflow_dispatch` event will restore the build cache.
    * Always save the build cache.
    * Reduced steps required when installing all packages through `install_all.sh`.
    * Cause `pyright` to install its associated NPM module during package install, making `actions/cache/save` cache the NPM module.
    * Remove the unused `matrix` strategy and use just an envvar for the Python version.
    
    Related #33

--- install_all.sh
+++ install_all.sh
@@ -2,15 +2,4 @@
 
 # use $1 to pass in -e (or any other options)
 
-python -m pip install $1 .
-
-python -m pip install $1 \
-    .[dev-dependencies] \
-    src/AWS \
-    src/database \
-    src/development \
-    src/GitHub \
-    src/platform \
-    src/programming \
-    src/testing \
-    src/web
+python -m pip install $1 .[dev-dependencies]
