43d7f92496d5d5d04a59276396c1d0282351a8ed
Aaron Holmes <aholmes@mednet.ucla.edu>
2024-02-15 17:40:26 -0800

    Add tests for DisallowDuplicateValues.
    Related #38

--- src/programming/test/unit/test_argparse.py
+++ src/programming/test/unit/test_argparse.py
@@ -1,7 +1,7 @@
-from argparse import ArgumentTypeError
+from argparse import ArgumentError, ArgumentParser, ArgumentTypeError, Namespace
 
 import pytest
-from BL_Python.programming.cli.argparse import disallow
+from BL_Python.programming.cli.argparse import DisallowDuplicateValues, disallow
 
 
 def test__disallow__raises_ArgumentTypeError_when_disallowed_value_is_used():
@@ -26,3 +26,35 @@ def test__disallow__returns_argument_value_as_custom_type_when_disallowed_value_
     value = argument_type("1")
 
     assert 2 == value
+
+
+def test__DisallowDuplicateValues__allows_repeated_use_of_argument():
+    class ParserNamespace(Namespace):
+        a: str  # pyright: ignore[reportUninitializedInstanceVariable]
+
+    parser = ArgumentParser()
+    argument_action = DisallowDuplicateValues(["-a"], "a")
+    namespace = ParserNamespace()
+
+    # the Action is called once for every specification of the argument
+    # this would be the equivalent of a command like `cmd -a value -a other_value`
+    _ = argument_action.__call__(parser, namespace, "value", "-a")
+    _ = argument_action.__call__(parser, namespace, "other_value", "-a")
+    assert isinstance(namespace.a, list)
+    assert "value" in namespace.a
+    assert "other_value" in namespace.a
+
+
+def test__DisallowDuplicateValues__raises_ArgumentError_when_duplicate_value_for_argument_is_used():
+    class ParserNamespace(Namespace):
+        a: str  # pyright: ignore[reportUninitializedInstanceVariable]
+
+    parser = ArgumentParser()
+    argument_action = DisallowDuplicateValues(["-a"], "a")
+    namespace = ParserNamespace()
+
+    with pytest.raises(ArgumentError):
+        # the Action is called once for every specification of the argument
+        # this would be the equivalent of a command like `cmd -a value -a value`
+        _ = argument_action.__call__(parser, namespace, "value", "-a")
+        _ = argument_action.__call__(parser, namespace, "value", "-a")
