686522beda3aa05de936c8fcd5b897ad94a195f7
Aaron Holmes <aholmes@mednet.ucla.edu>
2024-02-27 12:43:32 -0800

    Add further protections about Singleton behavior when `_block_changes` is `False`.
    - Also intentionally left in pyright error regarding positional
      arguments with the `__name` argument in the `__getattributes__`
      method. This will be indicated in a bug report w/ pyright if it is in
      fact a pyright error.

--- src/programming/BL_Python/programming/patterns/singleton.py
+++ src/programming/BL_Python/programming/patterns/singleton.py
@@ -27,6 +27,7 @@ class Singleton(type):
     To change this behavior, define an attribute named `_block_change` whose value is `False`.
     Not defining this attribute, or setting any other value, uses the default behavior.
     Be _VERY CAREFUL_ when using `_block_change = False` as `Singleton` is _NOT_ threadsafe.
+    Note also that it is not possible to prevent the deletion of _class_ attributes.
 
     For example, these are all equivalent:
     ```
@@ -48,6 +49,26 @@ class Singleton(type):
         _block_change = False
     ```
     """
+
+    class InstanceValue:
+        _value: Any
+        _deleted: bool
+
+        def __init__(self, value: Any) -> None:
+            self._value = value
+            self._deleted = False
+
+        def __get__(
+            self,
+            obj: _SingletonType | None,
+            objtype: type[_SingletonType] | None = None,
+        ):
+            if obj is not None:
+                return getattr(obj, "_value")
+            return self
+
+        def delete(self):
+            object.__setattr__(self, "_deleted", True)
 
     def __new__(
         cls: type,
@@ -77,14 +98,27 @@ class Singleton(type):
 
                 _new_type.__init__ = __init__
 
+                def __getattribute__(self: _SingletonType, __name: str) -> Any:
+                    value = super(cls, self).__getattribute__(__name)
+                    if isinstance(value, Singleton.InstanceValue):
+                        if value._deleted:
+                            raise AttributeError(self, __name)
+                        return value._value
+                    return value
+
                 def __setattr__(self: _SingletonType, name: str, value: Any):
                     if hasattr(self, block_change_attr_name) and self._block_change:
                         return
-                    object.__setattr__(self, name, value)
+
+                    if hasattr(cls, name):
+                        object.__setattr__(self, name, value)
+                    else:
+                        object.__setattr__(self, name, Singleton.InstanceValue(value))
 
                 def __delattr__(self: _SingletonType, name: str):
-                    if hasattr(self, block_change_attr_name) and self._block_change:
-                        return
+                    if hasattr(self, block_change_attr_name):
+                        if self.__getattribute__(block_change_attr_name):
+                            return
                     object.__delattr__(self, name)
 
                 _new_type.__setattr__ = __setattr__
--- src/programming/test/unit/patterns/test_singleton.py
+++ src/programming/test/unit/patterns/test_singleton.py
@@ -11,7 +11,7 @@ def test__multiple_instantiation_returns_same_instance():
     assert Foo() == Foo()
 
 
-def test__prevents_attribute_changes():
+def test__prevents_attribute_changes_by_default():
     class Foo(metaclass=Singleton):
         x: int = 123
 
@@ -53,7 +53,7 @@ def test__prevents_attribute_deletion_when_configured_with_blocking_values(
     assert Foo.x == 123
 
 
-def test__allows_attribute_changes_when_configured_with_non_blocking_value():
+def test__allows_attribute_changes_when_configured_with_non_blocking_value_when_block_change_is_false():
     class Foo(metaclass=Singleton):
         _block_change = False
         x: int = 123
@@ -67,7 +67,7 @@ def test__allows_attribute_changes_when_configured_with_non_blocking_value():
     assert Foo.x == 123
 
 
-def test__allows_attribute_deletion_when_configured_with_non_blocking_value():
+def test__allows_attribute_deletion_when_configured_with_non_blocking_value_when_block_change_is_false():
     class Foo(metaclass=Singleton):
         _block_change = False
         x: int = 123
@@ -88,3 +88,43 @@ def test__allows_attribute_deletion_when_configured_with_non_blocking_value():
     assert hasattr(Foo, "x")
     assert not hasattr(foo, "y")
     assert not hasattr(Foo, "y")
+
+
+@pytest.mark.parametrize("block_change", [True, False])
+def test__prevents_arbitrary_class_attribute_deletion_on_instances(block_change: bool):
+    class Foo(metaclass=Singleton):
+        _block_change = block_change
+        _arbitrary = True
+
+    foo = Foo()
+
+    # when _block_change is True, deletion is ignored
+    if block_change:
+        del foo._arbitrary  # pyright: ignore[reportPrivateUsage]
+        delattr(foo, "_arbitrary")
+    else:
+        with pytest.raises(AttributeError):
+            del foo._arbitrary  # pyright: ignore[reportPrivateUsage]
+
+        with pytest.raises(AttributeError):
+            delattr(foo, "_arbitrary")
+
+    assert hasattr(foo, "_arbitrary")
+    assert hasattr(Foo, "_arbitrary")
+
+
+@pytest.mark.parametrize("block_change", [True, False])
+def test__allows_arbitrary_class_attribute_deletion_on_classes(block_change: bool):
+    class Foo(metaclass=Singleton):
+        _block_change = block_change
+        _arbitrary = True
+
+    foo = Foo()
+
+    # This cannot be intercepted, even
+    # by the `__delete__` descriptor.
+    # As such, it is always allowed.
+    del Foo._arbitrary  # pyright: ignore[reportPrivateUsage]
+
+    assert not hasattr(foo, "_arbitrary")
+    assert not hasattr(Foo, "_arbitrary")
