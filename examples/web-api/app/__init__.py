c890f86bdd5ddbb61e57d510cf70c6d4a43d600f
Aaron Holmes <aholmes@mednet.ucla.edu>
2025-01-13 13:57:40 -0800

    Update web API example.

--- examples/web-api/app/__init__.py
+++ examples/web-api/app/__init__.py
@@ -1,11 +1,12 @@
 from Ligare.web.application import ApplicationBuilder
 from connexion import FlaskApp
 
-application_builder = ApplicationBuilder[FlaskApp]()
+application_builder = ApplicationBuilder(FlaskApp, __name__)
 
 application_builder.use_configuration(
     lambda config_builder: \
-        config_builder.with_config_filename("app/config.toml")
+        config_builder \
+            .with_config_filename("app/config.toml")
 )
 
 result = application_builder.build()
