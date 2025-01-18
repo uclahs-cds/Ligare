7b68c8d87f43e603fe223e5cb6c0f6e8fc8eaa15
Aaron Holmes <aholmes@mednet.ucla.edu>
2025-01-17 16:08:48 -0800

    Revise CLI application example in Sphinx docs.

--- examples/cli-app/app/__main__.py
+++ examples/cli-app/app/__main__.py
@@ -4,24 +4,25 @@ from Ligare.programming.config import Config
 from typing_extensions import override
 
 
-class AppConfig(Config):
-    message: str
-
-
 class Application(ApplicationBase):
     @inject
     @override
-    def run(self, config: AppConfig):
+    def run(self, config: "AppConfig"):
         print(config.message)
         input("\nPress anything to exit. ")
 
 
 builder = ApplicationBuilder(Application)
 
+
+class AppConfig(Config):
+    message: str
+
+
 builder.use_configuration(
-    lambda config_builder: config_builder.with_config_type(
-        AppConfig
-    ).with_config_filename("app/config.toml")
+    lambda config_builder: config_builder \
+        .with_config_type(AppConfig) \
+        .with_config_filename("app/config.toml")
 )
 
 application = builder.build()
--- sphinx-docs/source/cookbook/web_applications/create-application-from-scratch/index.rst
+++ sphinx-docs/source/cookbook/web_applications/create-application-from-scratch/index.rst
@@ -1,9 +1,9 @@
-Create an Application from Scratch
-==================================
+Create a Web Application from Scratch
+=====================================
 
 Let's create a web application that shows "Hello, World!"
 
-We will create this application from scratch, building each piece necessary for ``Ligare.web`` to function.
+We will create this application from scratch, building each piece necessary for your application to function.
 
 :example:`web-api`
 
