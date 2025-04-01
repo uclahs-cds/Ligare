0ec9ce1ab0d5d8817120c4da06fd53a01d8d8d67
Aaron Holmes <aholmes@mednet.ucla.edu>
2025-04-01 15:13:40 -0700

    Add module name and version to pypi workflow outputs and use for release name.

--- .github/workflows/PyPI-scripts/extract-package-details-from-tag.sh
+++ .github/workflows/PyPI-scripts/extract-package-details-from-tag.sh
@@ -4,6 +4,9 @@ tag="${GITHUB_REF#refs/*/}"
 
 # This is the package name minus `Ligare.`.
 package_name="$(gawk 'match($0, /Ligare\.?([^-]+)?/, m) { print s m[1]}' <<<"$tag")"
+module_version="$(gawk 'match($0, /Ligare\.?[^v]+v(.+)/, m) { print s m[1]}' <<<"$tag")"
+module_name="Ligare.$package_name"
+
 # `all` is the root of the repository and includes all other packages
 if [ "$package_name" == "all" ]; then
     package_directory=.
@@ -12,3 +15,5 @@ else
 fi
 echo "package_name=$package_name" >> "$GITHUB_OUTPUT"
 echo "package_directory=$package_directory" >> "$GITHUB_OUTPUT"
+echo "module_name=$module_name" >> "$GITHUB_OUTPUT"
+echo "module_version=$module_version" >> "$GITHUB_OUTPUT"
