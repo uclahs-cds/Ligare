bdce01b8a519fc776a56c4f2aa909947a9bab91f
Aaron Holmes <aholmes@mednet.ucla.edu>
2025-04-17 12:04:31 -0700

    Update R code to match lab style standards.

--- src/R/pythonipc/R/cli.R
+++ src/R/pythonipc/R/cli.R
@@ -2,47 +2,43 @@
 parse.cli.args <- function() {
   args <- commandArgs(trailingOnly = TRUE);
 
-  output.type.arg <- args[grep("--output-type=", args)];
+  output.type.arg <- args[grep('--output-type=', args)];
   output.type <- ifelse(
     identical(output.type.arg, character(0)),
-    "png",
-    sub(".*=", "", args)
+    'png',
+    sub('.*=', '', args)
   );
 
-  get.output.device.from.env <- function(.)
-  {
-    tryCatch(
-      {
-        output.fd.env <- as.integer(Sys.getenv("IMAGE_DATA_FD"))
-        if (is.na(output.fd.env)) stop("NA")
-        return(paste0("/dev/fd/", output.fd.env));
-      },
+  get.output.device.from.env <- function(.) {
+    tryCatch({
+        output.fd.env <- as.integer(Sys.getenv('IMAGE_DATA_FD'));
+        if (is.na(output.fd.env)) stop('NA');
+        return(paste0('/dev/fd/', output.fd.env));
+        },
       # envvar is not set, use stdout
-      warning = function(e) "/dev/stdout",
-      error = function(e) "/dev/stdout"
-    )
-  }
+      warning = function(e) '/dev/stdout',
+      error = function(e) '/dev/stdout'
+      );
+    }
 
-  get.output.device.from.args <- function(.)
-  {
-    tryCatch(
-      {
-        output.fd.arg.value <- as.integer(sub(".*=", "", output.fd.arg))
-        if (is.na(output.fd.arg.value)) stop("NA")
-        return(paste0("/dev/fd/", output.fd.arg.value))
-      },
+  get.output.device.from.args <- function(.) {
+    tryCatch({
+        output.fd.arg.value <- as.integer(sub('.*=', '', output.fd.arg));
+        if (is.na(output.fd.arg.value)) stop('NA');
+        return(paste0('/dev/fd/', output.fd.arg.value));
+        },
       # failed to parse the arg, try to get from env
       warning = get.output.device.from.env,
       error = get.output.device.from.env
-    )
-  }
+      );
+    }
 
-  output.fd.arg <- args[grep("--output-fd=", args)];
+  output.fd.arg <- args[grep('--output-fd=', args)];
   output.device <- ifelse(
     identical(output.fd.arg, character(0)),
     get.output.device.from.env(),
     get.output.device.from.args()
-  )
+    );
 
 
 
@@ -50,5 +46,5 @@ parse.cli.args <- function() {
     output.type = output.type,
     output.type.func = get(output.type),
     output.device = output.device
-  ));
-}
+    ));
+  }
