46e253c5539220fcd1f7091a2423b265b935b883
Aaron Holmes <aholmes@mednet.ucla.edu>
2025-04-18 11:11:12 -0700

    Add some documentation comments and refinement to R integration examples.

--- examples/R-integration/draw_plot.R
+++ examples/R-integration/draw_plot.R
@@ -1,21 +1,34 @@
+# nolint start: commented_code_linter.
 library("pythonipc");
 
-args <- parse.cli.args();
-output.type.func <- args$output.type.func;
-
-parameter.args <- read.method.parameters();
-letter_segments <- read.dataframe();
-
-on.exit(dev.off(), add = TRUE);
-
-invisible(pdf(NULL));
-invisible(output.type.func(args$output.device));
-
-letter_segments <- read.csv("letter_segments.csv");
-
-draw_ligare <- function(df, spacing = 1, line_width = 1, color = "black", background_color = "white") {
+# This method takes in a set of coordinates from a dataframe
+# and draws those coordinates as line segments.
+# For example, the letter "L" would be stored like this.
+# letter_segments <- data.frame(
+#  letter = c("L", "L"),
+#  x = c(0.0, 0.0),
+#  y = c(0.0, 0.0),
+#  xend = c(0.0, 0.6),
+#  yend = c(1.0, 0.0)
+# )
+draw_lines_from_dataframe <- function(
+  df,
+  spacing = 1,
+  line_width = 1,
+  color = "black",
+  background_color = "white"
+) {
   par(bg = background_color);
-  plot(NULL, xlim = c(0, 7), ylim = c(0, 1.1), asp = 1, axes = FALSE, xlab = "", ylab = "");
+  plot(
+    NULL,
+    xlim = c(0, 7),
+    ylim = c(0, 1.1),
+    asp = 1,
+    axes = FALSE,
+    xlab = "",
+    ylab = ""
+  );
+
   unique_letters <- unique(df$letter);
   for (i in seq_along(unique_letters)) {
     current_letter <- unique_letters[i];
@@ -29,4 +42,22 @@ draw_ligare <- function(df, spacing = 1, line_width = 1, color = "black", backgr
   }
 }
 
-do.call(draw_ligare, c(list(letter_segments), parameter.args));
+on.exit(dev.off(), add = TRUE);
+
+invisible(pdf(NULL));
+
+# get the output format and data FDs
+with(as.list(parse.cli.args()), {
+  invisible(output.type.func(output.device));
+})
+
+# read the method parameters for `draw_lines_from_dataframe`
+parameter_args <- read.method.parameters();
+# read the line segment data for `draw_lines_from_dataframe`
+letter_segments <- read.dataframe();
+
+# apply the line segment dataframe and other parameters
+# to the `draw_lines_from_dataframe` method
+do.call(draw_lines_from_dataframe, c(list(letter_segments), parameter_args));
+
+# nolint end
